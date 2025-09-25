42d311fa11c126e6b570c93510491718f782524e
Rahmat Ullah Afridi <afridirahmat09@gmail.com>
2025-09-25 22:32:56 +0500

    Update CoffeeBillingSystem.cpp

--- Task1/CoffeeBillingSystem.cpp
+++ Task1/CoffeeBillingSystem.cpp
@@ -1,1 +1,52 @@
+#include<iostream>
+using namespace std;
 
+
+int main(){
+    int equantity;
+    int lquantity;
+    int cquantity;
+
+    float espressoPrice = 150;
+    float *ePrice = &espressoPrice;
+
+    float lattePrice = 200;
+    float *lPrice = &lattePrice;
+
+    float cappucinoPrice = 250;
+    float *cPrice = &cappucinoPrice;
+
+    cout<<"Enter the espresso quantity : ";
+    cin>>equantity;
+    cout<<"Enter the latte quantity : ";
+    cin>>lquantity;
+    cout<<"Enter the cuppuccino : ";
+    cin>>cquantity;
+
+    // creating pointers for quantities
+    int *e = &equantity;
+    int *l = &lquantity;
+    int *c = &cquantity;
+
+    // pointers for calculating total prices of single single items
+    float eTotalPrice = *ePrice * *e; 
+    float *eTotal = &eTotalPrice;
+
+    float lTotalPrice = *lPrice * *l;
+    float *lTotal = &lTotalPrice;
+
+    float cTotalPrice = *cPrice * *c;
+    float *cTotal = &cTotalPrice;
+
+    // calculating sum as a whole 
+    float TotalSum = *eTotal + *lTotal + *cTotal;
+    float *tsum = &TotalSum;
+
+    cout<<" -----Cafe Bill----"<<endl;
+    cout<<"Espresso ("<<*e<<" X "<<*ePrice<<") = "<<*eTotal<<endl; 
+    cout<<"Latte ("<<*l<<" X "<<*lPrice<<") = "<<*lTotal<<endl; 
+    cout<<"Cappucino ("<<*c<<" X "<<*cPrice<<") = "<<*cTotal<<endl; 
+    cout<<"----------------------------"<<endl;
+    cout<<"The total = "<<*tsum<<endl;
+    return 0;
+}
