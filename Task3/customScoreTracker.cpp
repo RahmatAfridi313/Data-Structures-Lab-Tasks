1821467e712493eae175321e09ee1ac57640bda4
Rahmat Ullah Afridi <afridirahmat09@gmail.com>
2025-09-25 22:46:37 +0500

    Update customScoreTracker.cpp

--- Task3/customScoreTracker.cpp
+++ Task3/customScoreTracker.cpp
@@ -1,1 +1,56 @@
+#include<iostream>
+using namespace std;
 
+class ScoreTracker
+{
+    private:
+    int noOfPlayers;
+    int *scores;
+
+    public:
+    ScoreTracker(int size)
+    {
+        noOfPlayers = size;
+        scores = new int[noOfPlayers];
+        
+    }
+
+    ~ScoreTracker()
+    {
+        delete [] scores;
+    }
+
+    void getSocre()
+    {
+        for(int i=0; i<noOfPlayers; i++)
+        {
+            cout<<"Enter the score for player no."<<i+1<<" : ";
+            cin>>scores[i];
+            
+        }
+    }
+
+    void showSocre()
+    {
+        for(int i=0; i<noOfPlayers; i++)
+        {
+            cout<<"Player no."<<i+1<<" score is : "<<scores[i]<<endl;
+        }
+    }
+};
+
+int main(){
+
+    int players;
+    cout<<"Enter the number of players : ";
+    cin>>players;
+
+    ScoreTracker *scores = new ScoreTracker(players);
+    scores->getSocre();
+    scores->showSocre();
+
+    delete scores;
+
+
+    return 0;
+}
