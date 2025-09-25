50941de4d0acf9d3ff3157a8c1157316f014809f
Rahmat Ullah Afridi <afridirahmat09@gmail.com>
2025-09-25 22:42:14 +0500

    Update factorial.cpp

--- Task2/factorial.cpp
+++ Task2/factorial.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+using namespace std;
 
+int generateCode(int n)
+{
+    int fact = 1;
+    if(n<=0)
+    {
+        fact = 1;
+    }
+    else
+    {
+        for(int i=n; i>=1; i--)
+        {
+            fact = fact * i;
+        }
+    }
+    return fact;   
+}
+
+int(*ptrfact)(int) = &generateCode;
+int main()
+{
+    int num;
+    cout<<"Enter the number for factorial : ";
+    cin>>num;
+    //int factoial = generateCode(num);
+
+    int factoraiPtr = (*ptrfact)(num);
+    cout<<factoraiPtr<<endl;
+
+    return 0;
+}
